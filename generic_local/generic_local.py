2ceb98529eb4a42c6d5c570c3681b532e9fe7544
Hereward Cooper <coops@thepixiepeople.co.uk>
2012-03-02 20:52:06 +0000

    converted to int->float, added example for getting qps

--- generic_local/generic_local.py
+++ generic_local/generic_local.py
@@ -12,15 +12,19 @@
 
 # What's the command to run to get our raw data?
 #status_command="/usr/bin/varnishstat -1"
+status_command="mysql -e 'show status'"
+#status_command="mysqladmin status | sed 's/  /\\n/g' | sed 's/ /_/g' | sed 's/:_/ /g'"
+
 
 # What's a human-friendly prefix to name our checks?
 #prefix = "Varnish_"
+prefix = "MySQL_"
 
 # What variables shall we actually check for? Format:
 # ("variable name", warning threshold, critical threshold)
 checks = [
-#	( "backend_fail", 10, 100),
-#	( "client_conn", 40, 100)
+#   ( "Queries_per_second_avg", 1, 5),
+   ( "Slow_queries", 1, 5),
 ]
 
 #------------------------------------------------------------
@@ -48,17 +52,16 @@ def debug():
                         sys.stdout.write("VAR: " + line.split()[0])
                 except:
                         continue
-       
                 try:
-        	        print " - " + line.split()[1]
-	        except:
-        	        continue
+                    print " - " + line.split()[1]
+                except:
+                    continue
 
         print "----------------"
         print "DEBUG: My Checks"
         print "----------------"
         for check,warn,crit in checks:
-        	print check, warn, crit
+            print check, warn, crit
 
 #------------------------------------------------------------
 # THE MAGIC
@@ -70,33 +73,29 @@ def output(type, check, value):
 
 # Read through each line in the output of SHOW STATUS
 def run_checks():
-        for line in status.split('\n'):
-        
-                # Sometimes we have a mare reading the first and last lines. Skip if needed.
+        for line in status.splitlines():
+            # Sometimes we have a mare reading the first and last lines. Skip if needed.
                 try:
-                        var = line.split()[0]
+                    var = line.split()[0]
                 except:
-                        continue
+                    continue
         
                 # Some variables don't actually have a value set. Handle this (skip for now).
                 try:
-                        var_value = int(line.split()[1])
+                    var_value = float(line.split()[1])
                 except:
-                        continue
+                    continue
         
                 # Read through each of our configured variables to check
                 for check,warn,crit in checks:
-        
-        		# Is the variable one on the list to check?
-        		if var == check:
-        
-        			# is the current value above the various thresholds? If so output.
-        			if var_value > crit:
-        			    output(2,var,"CRITICAL: " + prefix + var + " " + str(var_value))
-        			elif var_value > warn:
-        			    output(1,var,"WARNING: " + prefix + var + " " + str(var_value))
-        			else:
-        			    output(0,var,"OK: " + prefix + var + " " + str(var_value))
+                    # Is the variable one on the list to check?
+                    if var == check:
+                        if var_value > crit:
+                            output(2,var,"CRITICAL: " + prefix + var + " " + str(var_value))
+                        elif var_value > warn:
+                            output(1,var,"WARNING: " + prefix + var + " " + str(var_value))
+                        else:
+                            output(0,var,"OK: " + prefix + var + " " + str(var_value))
 
 # Actually do some work!
 run_checks()
