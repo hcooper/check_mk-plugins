edfd62feee79c61fb71cdb55819b38121bf0f08f
Hereward Cooper <coops@thepixiepeople.co.uk>
2012-09-14 09:08:37 +0100

    rewrite of logic

--- lsi_megariad/lsi_megaraid.py
+++ lsi_megariad/lsi_megaraid.py
@@ -11,22 +11,17 @@ def inventory_lsi_megaraid(checkname, info):
 def check_lsi_megaraid(item, params, info):
 
     # Take the results array, and split it into each of our attributes
-    vdDegradedCount, vdOfflineCount, pdDiskFailedCount, pdDiskPredFailureCount = info[0]
+    checks == ['vdDegradedCount', 'vdOfflineCount', 'pdDiskFailedCount', 'pdDiskPredFailureCount']
+    results = dict(zip(checks, info[0]))
 
-    # Do the logic to determine if we should alert or not
-    if vdDegradedCount != "0":
-        return (2, "CRITICAL - vdDegradedCount > 0")
-    elif vdOfflineCount != "0":
-        retrun (2, "CRITICAL - vdOfflineCount > 0")
-    elif pdDiskFailedCount != "0":
-        return (2, "CRITICAL - pdDiskFailedCount > 0")
-    elif pdDiskPredFailureCount != "0":
-        return (2, "CRITICAL - pdDiskPredFailureCount > 0")
-    else:
-        return (0, "OK - no reported errors")
+    for check in results:
+        if results[check] == 0:
+            continue
+        else:
+            return (2, "CRITICAL - %s: %s" % (check, results[check]))
+
+    return (0, "OK - no reported errors")
 
 
 check_info["lsi_megaraid"] = (check_lsi_megaraid, "LSI MegaRAID", 0, inventory_lsi_megaraid)
-
 snmp_info["lsi_megaraid"] = ( ".1.3.6.1.4.1.3582.4.1.4.1.2.1", ["19", "20", "24", "23"] )
-
