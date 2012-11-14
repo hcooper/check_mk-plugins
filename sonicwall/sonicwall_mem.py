c9b0729d7b657e96fa9346e0debd45123aa3cc46
Hereward Cooper <coops@thepixiepeople.co.uk>
2012-11-14 10:41:30 +0000

    fix mem issues

--- sonicwall/sonicwall_mem.py
+++ sonicwall/sonicwall_mem.py
@@ -1,26 +1,31 @@
 #!/usr/bin/env python
 
-# Monitoring the memory of a sonicwall
+# Monitoring the Mem usage of a Sonicwall
 # Herward Cooper <coops@fawk.eu> - 2012
 
-# Used OID 1.3.6.1.4.1.8741.1.3.1.4.0
+# Uses OID 1.3.6.1.4.1.8741.1.3.1.4.0
+
+sonicwall_mem_default_values = (35, 40)
 
 def inventory_sonicwall_mem(checkname, info):
     inventory=[]
     status = int(info[0][0])
     if status < 11:
-        inventory.append( (None, None) )
+        inventory.append( (None, None, "sonicwall_mem_default_values") )
     return inventory
 
 
 def check_sonicwall_mem(item, params, info):
+    warn, crit = params
     state = int(info[0][0])
-    if state == 8:
-        return (0, "OK - Peer is compatible")
+    perfdata = [ ( "cpu", state, warn, crit ) ]
+    if state > crit:
+        return (2, "CRITICAL - Mem is %s percent" % state, perfdata)
+    elif state > warn:
+        return (1, "WARNING - Mem is %s percent" % state, perfdata)
     else:
-        return (2, "CRITICAL - Peer is not compatible!")
-    return (3, "UNKNOWN - unhandled problem")
+        return (0, "OK - Mem is %s percent" % state, perfdata)
 
-check_info["sonicwall_mem"] = (check_sonicwall_mem, "Sonicwall Memory", 0, inventory_sonicwall_mem)
+check_info["sonicwall_mem"] = (check_sonicwall_mem, "Sonicwall Mem", 1, inventory_sonicwall_mem)
 
-snmp_info["sonicwall_mem"] = ( "1.3.6.1.4.1.8741.1.3.1.4", [ "0" ] )
+snmp_info["sonicwall_mem"] = ( ".1.3.6.1.4.1.8741.1.3.1.4", [ "0" ] )
